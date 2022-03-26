7faf3118bbb2811f38ed75cf0d5432cb45a993b2
CyberAlexander <taurum13@gmail.com>
2022-03-26 09:11:47 +0100

    Initial service and repository layers structure added.

---
+++ src/main/java/com/einarr/maximus/helloworld/repository/UserRepositoryImpl.java
@@ -0,0 +1,13 @@
+package com.einarr.maximus.helloworld.repository;
+
+/**
+ * Created : 26/03/2022 09:10
+ * Project : Hello-World
+ * IDE : IntelliJ IDEA
+ *
+ * @author alexanderleonovich
+ * @version 1.0
+ */
+public class UserRepositoryImpl implements UserRepository {
+
+}
---
+++ src/main/java/com/einarr/maximus/helloworld/service/GreetingServiceImpl.java
@@ -0,0 +1,13 @@
+package com.einarr.maximus.helloworld.service;
+
+/**
+ * Created : 26/03/2022 09:09
+ * Project : Hello-World
+ * IDE : IntelliJ IDEA
+ *
+ * @author alexanderleonovich
+ * @version 1.0
+ */
+public class GreetingServiceImpl implements GreetingService {
+
+}
