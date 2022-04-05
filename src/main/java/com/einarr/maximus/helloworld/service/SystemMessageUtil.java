78ff02b1d10af96d2ac349417299a4669d52fad8
Einarr-Maximus <einarr.maximus@gmail.com>
2022-04-06 00:07:59 +0300

    Add Test.java file for get MAC address (not work) (#21)
    pom.xml changes

---
+++ src/main/java/com/einarr/maximus/helloworld/service/SystemMessageUtil.java
@@ -0,0 +1,91 @@
+package com.einarr.maximus.helloworld.service;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class SystemMessageUtil {
+    /**
+     * Получить имя операционной системы:
+     *
+     * @return
+     */
+    private static String getOSName() {
+        return System.getProperty("os.name").toLowerCase();
+    }
+
+    /**
+     * Получить локальный IP-адрес;
+     *
+     * @return
+     */
+    private String getLocalIpAddress() {
+        String ipAddress = null;
+        try {
+            ipAddress = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return ipAddress;
+    }
+
+    /**
+     * Получить MAC-адрес этой машины;
+     *
+     * @return
+     */
+    public String getLocalWindosMacAddress() {
+        String mac = null;
+        BufferedReader bufferReader = null;
+        Process process = null;
+        try {
+            // Отображение информации об адресе сетевой карты Mac под windows;
+            process = Runtime.getRuntime().exec("ipconfig /all");
+
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        bufferReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+        String line = null;
+        int index = -1;
+        try {
+            while ((line = bufferReader.readLine()) != null) {
+                System.out.println(line);
+                index = line.indexOf("Physical Address");
+
+                if (index >= 0) {
+                    index = line.indexOf(":");
+                    if (index >= 0) {
+                        mac = line.substring(index + 1).trim();
+                    }
+                }
+            }
+
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } finally {
+            try {
+                if (bufferReader != null) {
+                    bufferReader.close();
+                }
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+        return mac;
+    }
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        SystemMessageUtil messageUtil = new SystemMessageUtil();
+        System.out.println(messageUtil.getLocalIpAddress());
+
+    }
+}
